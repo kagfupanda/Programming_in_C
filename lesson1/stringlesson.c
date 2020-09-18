7a5b33e77d4dc09515ebb16aea095644441a7019
kagfupanda <kagfupanda@gmail.com>
2020-09-18 14:39:11 -0700

    added stringlesson

---
+++ lesson1/stringlesson.c
@@ -0,0 +1,36 @@
+/*
+* Author: siddharth
+* Simple program to use C library for string manipulation
+*/
+#include <stdio.h>
+#include <string.h> /* to use string library functions */
+
+/*
+* Concatenate the two strigns provided as arguments. and output the concatenated string
+* Example:
+*	a.out abcd xyz
+* Output printed should be: abcdxyz
+*/
+int main(int argc, char **argv)
+{
+	/* Input strings are in argv*/
+	/* argv[0] index is program name */
+	/* First stringn is in argv index 1, argv[1] */
+	/* second input strings in argv index 2, argv[1] */
+	if (argc != 3) {
+		printf("Insufficient arguments. Invalid syntax. \n");
+		printf("Usage: %s <string1> <string2>\n", argv[0]);
+		return -1;
+	}
+	
+	/* strcat() is C library function to concatenate strings */
+	/* assume maximum string size of 128 */
+	char result_string[128]; /* buffer to use to concatenate strings */
+	(void)strcpy(result_string, argv[1]); /* copy first string into result buffer */
+	strcat(result_string, argv[2]); /* append second string */
+
+	/* output the result string */
+	printf("%s\n", result_string);
+	
+	return 0;
+}
